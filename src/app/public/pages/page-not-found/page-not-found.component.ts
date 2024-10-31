a788695fc4c6e4b8aacda4867248c98a8dd02b53
estefanojaque <u202225466@upc.edu.pe>
2024-10-31 10:28:48 -0500

    fix: Update page-not-found

--- src/app/public/pages/page-not-found/page-not-found.component.ts
+++ src/app/public/pages/page-not-found/page-not-found.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+	import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-page-not-found',
-  standalone: true,
-  imports: [],
-  templateUrl: './page-not-found.component.html',
-  styleUrl: './page-not-found.component.css'
-})
-export class PageNotFoundComponent {
+	import { TranslateService } from "@ngx-translate/core";
+	import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
-}
+	@Component({
+	  selector: 'app-language-switcher',
+	  standalone: true,
+	  imports: [MatButtonToggleModule],
+	  templateUrl: './language-switcher.component.html',
+	  styleUrl: './language-switcher.component.css'
+	})
+	export class LanguageSwitcherComponent {
+	currentLang: string = 'en';
+	languages: string[] = ['en', 'es'];
+
+	constructor(private translate: TranslateService) {
+	  this.currentLang = translate.currentLang;
+	}
+
+	useLanguage(language: string) : void {
+	  this.translate.use(language);
+	}
+	}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
-export const environment = {
-    production: true,
-    serverBasePath: undefined
-  };
+export const environment = {production: true,
+                            //serverBasePath: "https://my-json-server.typicode.com/estefanojaque/Managewise-reportes"
+                            serverBasePath: "http://localhost:3000"
+                            };
+
